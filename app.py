58ebfeb8fb8e37d92545c866bfdb8779c8688fc6
HAL22 <thethelafaltein@gmail.com>
2023-11-19 17:48:35 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import qa
+import time
+
+st.title("Query any Youtube video")
+
+submitted = False
+with st.form(key='txt_input'):
+    txt_input = st.text_area('Enter youtube url', '', height=80)
+    submit_button = st.form_submit_button(label='Enter')
+    if submit_button:
+        qa.load_pinecone(txt_input,400)
+        submitted = True
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+    if submitted:
+        assistant_response = qa.qa_answer(prompt)
+    else:
+        assistant_response = ""
+
+    # Simulate stream of response with milliseconds delay
+    for chunk in assistant_response.split():
+        full_response += chunk + " "
+        time.sleep(0.05)
+        # Add a blinking cursor to simulate typing
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": full_response})         
+
+          
---
+++ qa.py
@@ -0,0 +1,131 @@
+from pytube import YouTube
+import whisper
+import pandas as pd
+import math
+import tiktoken
+import os
+import pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain.schema import Document
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+import streamlit as st
+
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+def get_video_transcription(video_url):
+    audio_file = YouTube(video_url).streams.filter(only_audio=True).first().download(filename="audio.mp4")
+
+    whisper_model = whisper.load_model("tiny")
+
+    transcription = whisper_model.transcribe(audio_file)
+
+    return transcription
+
+
+
+def get_text_chunks_metadata(video_url,chunk_size=100):
+    transcription = get_video_transcription(video_url)
+    df = pd.DataFrame(transcription['segments'], columns=['start', 'end', 'text'])
+
+    texts = []
+    metadata = []
+    row_size = df.shape[0]
+    i = 0
+
+    while i < row_size:
+        text = ""
+        size_counter = 0
+        row = df.iloc[i]
+        start = math.floor(row['start'])
+        end = math.floor(row['end'])
+        while i < row_size and size_counter <= chunk_size:
+            row = df.iloc[i]
+            t = row['text']
+            end = math.floor(row['end'])
+            size_counter += len(t)
+            text = text + " " + t
+            i+=1
+    
+        texts.append(text)
+        mData = {
+            "start":start,
+            "end": end
+        }
+        metadata.append(mData)
+
+    return texts, metadata
+
+
+def load_pinecone(video_url,chunk_size=100,embeddings=OpenAIEmbeddings(model="text-embedding-ada-002")):
+    # initialize pinecone
+    pinecone.init(
+    api_key=st.secrets['PINECONE_API_KEY'],
+    environment=st.secrets['PINECONE_ENV']
+    )
+    index_name = st.secrets['PINECONE_NAME']
+
+    if index_name not in pinecone.list_indexes():
+         # we create a new index
+        pinecone.create_index(
+            name=index_name,
+            dimension=1536  
+        )
+    else:
+        return Pinecone.from_existing_index(index_name,embeddings)
+
+    texts, metadata = get_text_chunks_metadata(video_url,chunk_size)
+    docs = []
+    for i in range(len(texts)):
+        doc = Document(
+        page_content=texts[i],
+        metadata=metadata[i]
+        )
+        docs.append(doc)
+
+    return Pinecone.from_documents(docs, embeddings, index_name=index_name)    
+
+def get_similiar_docs(query,index,k=2, score=False):
+    if score:
+        similar_docs = index.similarity_search_with_score(query, k=k)
+    else:
+        similar_docs = index.similarity_search(query, k=k)
+    return similar_docs
+
+
+def get_index():
+    return load_pinecone("",400)
+
+def qa_answer(query):
+    index = get_index()
+
+    model_name = "gpt-3.5-turbo"
+    llm = OpenAI(model_name=model_name)
+    chain = load_qa_chain(llm, chain_type="stuff")
+
+    similar_docs_1 = get_similiar_docs(query,index,4,False)
+
+    similar_docs_2 = get_similiar_docs(query,index,4,True)
+
+    doc_start = similar_docs_2[0][0].metadata
+
+    doc_end = similar_docs_2[3][0].metadata
+
+    start = math.floor(doc_start["start"])
+
+    end = math.floor(doc_end["end"])
+
+    answer = chain.run(input_documents= similar_docs_1 , question=query)
+
+    value = answer + "\n" + f"You can watch it from start {start} to {end}"
+
+    return value, start, end
+
+
+
+
+
